59903958ba3274ca10614ca7b0976c6e436148bf
Diego Guadarrama <diegoguadarrama@gmail.com>
2025-01-13 00:44:06 -0600

    Update Header.tsx
    Removed Sign Up button and swtiched text from Log In to Open App

--- src/components/landing/Header.tsx
+++ src/components/landing/Header.tsx
@@ -16,13 +16,7 @@ export default function Header() {
             className="bg-white text-primary hover:bg-gray-200"
             onClick={() => window.location.href = '/login'}
           >
-            Log In
-          </Button>
-          <Button 
-            className="bg-white text-primary hover:bg-gray-200"
-            onClick={() => window.location.href = '/login'}
-          >
-            Sign Up
+            Open App
           </Button>
         </div>
       </nav>
