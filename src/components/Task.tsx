843f2c0b8c0460725b2a5cd6970d0164c52c0cd1
Diego Guadarrama <diegoguadarrama@gmail.com>
2025-01-18 23:57:46 -0600

    Update Task.tsx
    px-2 py-1 bg-red-100 text-red-700 text-sm rounded

--- src/components/Task.tsx
+++ src/components/Task.tsx
@@ -148,9 +148,9 @@ export const Task = ({ task, isDragging, onTaskClick, onTaskUpdate }: TaskProps)
             <div 
               className={`
                 inline-flex px-2 py-0.5 rounded-full text-xs font-medium border
-                ${task.priority === 'high' ? 'text-red-700 border-red-700 dark:border-red-500 dark:text-red-500' : ''}
-                ${task.priority === 'medium' ? 'text-yellow-500 border-yellow-500 dark:border-yellow-500 dark:text-yellow-500' : ''}
-                ${task.priority === 'low' ? 'text-green-700 border-green-700 dark:border-green-500 dark:text-green-500' : ''}
+                ${task.priority === 'high' ? 'px-2 py-1 bg-red-100 text-red-700 text-sm rounded dark:border-red-500 dark:text-red-500' : ''}
+                ${task.priority === 'medium' ? 'px-2 py-1 bg-yellow-100 text-yellow-700 text-sm rounded dark:border-yellow-500 dark:text-yellow-500' : ''}
+                ${task.priority === 'low' ? 'px-2 py-1 bg-gray-100 text-sm rounded dark:border-green-500 dark:text-green-500' : ''}
               `}
             >
               {task.priority.charAt(0).toUpperCase() + task.priority.slice(1)}
