3c4710db3688f4a15182ffcbeda3ed6922132faa
Diego Guadarrama <diegoguadarrama@gmail.com>
2025-01-21 16:06:28 -0600

    Update index.ts

--- supabase/functions/create-checkout-session/index.ts
+++ supabase/functions/create-checkout-session/index.ts
@@ -144,7 +144,7 @@ serve(async (req) => {
       payment_method_types: ['card'],
       automatic_tax: { enabled: true },
       tax_id_collection: { enabled: true },
-      allow_promotion_codes: true
+      allow_promotion_codes: true,
       customer_update: {
         address: 'auto',
         shipping: 'auto'
