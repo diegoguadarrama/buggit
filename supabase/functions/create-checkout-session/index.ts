b875070ee72c9573145b58bcdeb4096ae296d374
Diego Guadarrama <diegoguadarrama@gmail.com>
2025-01-22 10:14:41 -0600

    Update index.ts

--- supabase/functions/create-checkout-session/index.ts
+++ supabase/functions/create-checkout-session/index.ts
@@ -43,7 +43,7 @@ serve(async (req) => {
     const { priceId, createPortalSession } = await req.json();
 
     const stripe = new Stripe(Deno.env.get('STRIPE_SECRET_KEY') || '', {
-      apiVersion: '2023-10-16',
+      apiVersion: '2024-12-18',
     });
 
     // Handle portal session creation
