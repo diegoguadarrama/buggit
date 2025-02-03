c549eef240e0c6bce142ff6f44e0f55750c316e3
Diego Guadarrama <diegoguadarrama@gmail.com>
2025-02-03 16:18:57 -0600

    Update Blog.tsx

--- src/pages/Blog.tsx
+++ src/pages/Blog.tsx
@@ -44,7 +44,7 @@ export default function Blog() {
     },
   });
   
-  if (isLoading) {
+  if (isLoadingFeatured || isLoadingPosts) {
     return (
       <div className="min-h-screen flex flex-col">
         <Header />
